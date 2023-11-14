4bb803a4aaa38adf448c2e2fd8c29c986eaf40c4
LukeWalls22 <dimuroluca@hotmail.it>
2023-11-14 13:01:13 +0100

    Prove link index

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let checkbox = document.querySelector("#checkbox");
+console.dir(checkbox.checked)
+
+let button = document.querySelector("#proceedbtn");
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (checkbox.checked === true){
+    location.href = "../index2.html"        //Da modificare
+    }
+})
+
