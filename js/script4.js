d369fa9c5bebecc4ebe99870995dbf3845d5d5fc
LukeWalls22 <dimuroluca@hotmail.it>
2023-11-16 16:06:31 +0100

    Aggiornamenti pagina 4

--- js/script4.js
+++ js/script4.js
@@ -2,4 +2,16 @@
 /* sto provando ad inserire le stelline mentre questa parla */
 
 
+//
 
+let contenitoreStelle = document.querySelector("#starRating");
+let button = document.querySelector("button");
+let starsLeft = document.querySelector("#starsLeft");
+
+//Nel "contenitoreStelle" prendi ogni elemento (stella) aggiungi un Event Listener che vede l'hover e aggiunge la classe "starHover"(che serve a colorarla)
+
+contenitoreStelle.forEach(stella => {
+    stella.addEventListener("mouseover", () => {
+        stella.classList.add("starHover");
+    })
+});
